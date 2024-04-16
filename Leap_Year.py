60ea275f247a2b4831217a242b05d86906e16a1f
Chaitanya Gupta <35136384+Gupta776@users.noreply.github.com>
2024-04-15 23:09:09 -0500

    Create Leap_Year.py

---
+++ Leap_Year.py
@@ -0,0 +1,14 @@
+#Write a function to determine if a given year is a leap year or not.
+
+def leap_year_validation(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+year = 2024
+if leap_year_validation(year):
+    print(f"{year} - leap year")
+else:
+    print(f"{year} - not a leap year.")
