d5d5cfc871c60e8d72064dc1d6e1f1c6d6943f01
Chaitanya Gupta <35136384+Gupta776@users.noreply.github.com>
2024-04-29 23:58:48 -0500

    Update 2073.py

--- 2073.py
+++ 2073.py
@@ -4,7 +4,7 @@ class Solution:
         # for i in range(tickets[k]):
         while tickets[k] > 0:
             v = 0
-            # for j in tickets:
+            # for j in tickets: 
             for j in range(len(tickets)):
                 # if j > 0:
                 if tickets[j] != 0:
